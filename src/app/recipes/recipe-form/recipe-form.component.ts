742802eb634fe7b89238f8e3dfa23762c21636ae
LinkinGeorge <georgdonner98@gmail.com>
2017-04-18 15:13:22 +0100

    Added showPicker function and filestack constant (doesn't work yet)

--- src/app/recipes/recipe-form/recipe-form.component.ts
+++ src/app/recipes/recipe-form/recipe-form.component.ts
@@ -2,6 +2,12 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe";
 import { Ingredient } from "../ingredient";
 
+declare const filestack: {
+  init(apiKey: string): {
+    pick({ maxFiles }: { maxFiles: number}): Promise<{ filesUploaded: { url: string }[] }> 
+  }
+};
+
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
@@ -9,6 +15,7 @@ import { Ingredient } from "../ingredient";
 })
 export class RecipeFormComponent {
 
+  uploadedFileUrls: string[] = [];
   ingredients = new Array<Ingredient>();
   newIngredient = new Ingredient('', '');
   model = new Recipe('', 0, 0, this.ingredients, '');
@@ -29,6 +36,17 @@ export class RecipeFormComponent {
         this.ingredients.push(ingr);
         this.newIngredient = new Ingredient('', '');
       }
+  }
+
+  hello() {
+    console.log('hello');
+  }
+
+  async showPicker() {
+    const client = filestack.init('AwD48ceQaWtGBs9plMog7z');
+    const result = await client.pick({ maxFiles: 1 });
+    const url = JSON.stringify(result.filesUploaded[0].url);
+    this.uploadedFileUrls.push(url);
   }
 
 }
