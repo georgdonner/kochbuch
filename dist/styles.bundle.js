3e3125edaaf533fdbf3c08ad4375c5b522d39adc
LinkinGeorge <georgdonner98@gmail.com>
2017-04-24 22:29:41 +0100

    Give bottom buttons margin and change section headers.

--- dist/styles.bundle.js
+++ dist/styles.bundle.js
@@ -65,7 +65,7 @@ exports = module.exports = __webpack_require__(12)();
 
 
 // module
-exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n.click {\r\n    cursor: pointer;\r\n}", ""]);
+exports.push([module.i, "/* You can add global styles to this file, and also import other style files */\r\n.click {\r\n    cursor: pointer;\r\n}\r\n\r\n.section-header {\r\n  font-weight: 400;\r\n}", ""]);
 
 // exports
 
