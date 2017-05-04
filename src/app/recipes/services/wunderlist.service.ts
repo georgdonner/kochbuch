081adf54abcd007cc35fd42b0b5f37e125d0119f
LinkinGeorge <georgdonner98@gmail.com>
2017-05-04 23:54:14 +0100

    Append access control header.

--- src/app/recipes/services/wunderlist.service.ts
+++ src/app/recipes/services/wunderlist.service.ts
@@ -12,6 +12,7 @@ export class WunderlistService {
     console.log(JSON.stringify(code));
     var headers = new Headers();
     headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', 'true');
     return this.http.post('https://www.wunderlist.com/oauth/access_token', {
       "client_id": "b4ab80671b8df7024e4d",
       "client_secret": "d3249ae86687441fdb19203c82c9c339b7052384155677ba2492f523f5b9",
