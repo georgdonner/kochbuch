9370cefd931788de792f56a949a773d132988906
LinkinGeorge <georgdonner98@gmail.com>
2017-05-05 00:11:16 +0100

    Update access control header to *.

--- src/app/recipes/services/wunderlist.service.ts
+++ src/app/recipes/services/wunderlist.service.ts
@@ -12,7 +12,7 @@ export class WunderlistService {
     console.log(JSON.stringify(code));
     var headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('Access-Control-Allow-Origin', 'http://georgs-recipes.herokuapp.com');
+    headers.append('Access-Control-Allow-Origin', '*');
     console.log(headers);
     return this.http.post('https://www.wunderlist.com/oauth/access_token', {
       "client_id": "b4ab80671b8df7024e4d",
