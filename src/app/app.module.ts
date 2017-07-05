ad6ecce8e7d1b569dd7737e0849109f16e16c43f
LinkinGeorge <georgdonner98@gmail.com>
2017-07-05 22:34:03 +0100

    Clean up repo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,11 @@ import { MarkdownModule } from 'angular2-markdown';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './recipes/recipes.module';
-import { SvgIconComponent } from './svgicons/svg-icon/svg-icon.component';
 import { PageNotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SvgIconComponent,
     PageNotFoundComponent
   ],
   imports: [
