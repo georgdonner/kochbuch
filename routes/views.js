5c3ae456fc5e933f71ca078972473eec292ba9ca
Georg Donner <georgdonner98@gmail.com>
2018-11-23 15:28:07 +0100

    Add static view for recipe edit.

--- routes/views.js
+++ routes/views.js
@@ -5,6 +5,16 @@ const router = express.Router();
 
 const Recipe = require('../models/recipe');
 
+router.get('/recipe/:id/edit', async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) throw new Error('Recipe not found.');
+    res.render('recipe-form', { recipe });
+  } catch (error) {
+    res.send(error);
+  }
+});
+
 router.get('/recipe/:id', async (req, res) => {
   try {
     const recipe = await Recipe.findById(req.params.id);
