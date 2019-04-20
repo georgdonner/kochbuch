a4aa731efb480b4c7dfe45a626d5b33c7a228ed4
Georg Donner <georgdonner98@gmail.com>
2019-04-20 23:36:17 +0100

    Allow buttons to be links in the navigation

--- static/scripts/src/modules/nav-menu.mjs
+++ static/scripts/src/modules/nav-menu.mjs
@@ -1,25 +1,32 @@
-const addButton = (icon, onClick, hasActiveState = false) => {
+const addButton = ({
+  icon, onClick, link, hasActiveState = false,
+}) => {
   const buttonContainers = document.getElementsByClassName('right-buttons');
   Array.from(buttonContainers).forEach((buttonContainer) => {
-    const button = document.createElement('button');
+    const button = link ? document.createElement('a') : document.createElement('button');
+    button.className = 'button';
     button.innerHTML = `
     <svg class="icon icon-${icon}">
       <use xlink:href="#icon-${icon}"></use>
     </svg>
     `;
-    button.addEventListener('click', (e) => {
-      e.preventDefault();
-      onClick(button);
-      if (hasActiveState) {
-        button.classList.toggle('active');
-      }
-    });
+    if (link) {
+      button.href = link;
+    } else {
+      button.addEventListener('click', (e) => {
+        e.preventDefault();
+        onClick(button);
+        if (hasActiveState) {
+          button.classList.toggle('active');
+        }
+      });
+    }
     buttonContainer.appendChild(button);
   });
 };
 
 export default (buttons) => {
   buttons.forEach((button) => {
-    addButton(button.icon, button.onClick, button.hasActiveState);
+    addButton(button);
   });
 };
