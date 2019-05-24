4aa9b8c0e68a38f791b1759d1e09215968a2cd66
Georg Donner <georgdonner@posteo.de>
2019-05-24 17:43:53 +0100

    Display full matches first when searching

--- static/scripts/src/modules/recipes-db.mjs
+++ static/scripts/src/modules/recipes-db.mjs
@@ -76,12 +76,20 @@ export const getRecipes = async (query) => {
   const terms = searchQuery.split(/,\s*/)
     .map(term => term.toLowerCase().trim())
     .map(term => SEARCH_MAP[term] || term);
-  const regexTerms = terms.map(term => new RegExp(`\\S*${term.slice(0, -1)}\\S*`, 'i'));
+  const regexTerms = terms.map(term => new RegExp(`\\S*${term}\\S*`, 'i'));
+  const regexTermsShort = terms.map(term => new RegExp(`\\S*${term.slice(0, -1)}\\S*`, 'i'));
   const matches = recipes
     .map(recipe => ({ recipe, score: searchScore(recipe, terms, regexTerms) }))
     .filter(({ score }) => score > 0);
   matches.sort((a, b) => b.score - a.score);
-  return matches.map(({ recipe }) => recipe);
+  const matchesShort = recipes
+    .map(recipe => ({ recipe, score: searchScore(recipe, terms, regexTermsShort) }))
+    .filter(({ score }) => score > 0);
+  matchesShort.sort((a, b) => b.score - a.score);
+  const allMatches = matches.concat(
+    matchesShort.filter(({ recipe }) => !matches.find(match => recipe._id === match.recipe._id)),
+  );
+  return allMatches.map(({ recipe }) => recipe);
 };
 
 const addRecipe = (recipe, db) => new Promise((resolve, reject) => {
