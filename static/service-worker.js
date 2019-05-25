55a6e503f13eceadddbc4a587cb3910b9b6a3659
Georg Donner <georgdonner@posteo.de>
2019-05-25 22:45:27 +0100

    Update service worker to cache things smarter

--- static/service-worker.js
+++ static/service-worker.js
@@ -23,13 +23,7 @@ const openDb = () => new Promise((resolve, reject) => {
 importScripts('/scripts/recipe-template.js');
 
 self.addEventListener('activate', (e) => {
-  const assets = [
-    '/css/recipe.css', '/scripts/src/recipe.js',
-    '/list', '/css/list.css', '/scripts/src/list.js',
-    '/scripts/src/modules/calc-servings.mjs', '/scripts/src/modules/toast.mjs',
-    '/scripts/src/modules/nav-menu.mjs',
-    '/offline',
-  ];
+  const assets = ['/offline'];
   e.waitUntil(
     caches
       .open(cacheName)
@@ -91,11 +85,14 @@ function fetchRequest(request) {
       return res;
     })
     .catch(() => caches.match(request).then((res) => {
+      if (res) {
+        return res;
+      }
       const url = new URL(request.url);
       // eslint-disable-next-line no-undef
-      if (!res && isRecipeView(url) && 'template' in self) {
+      if (isRecipeView(url) && 'template' in self) {
         return renderRecipe(url);
-      } if (!res && url.origin === 'https://ucarecdn.com') {
+      } if (url.origin === 'https://ucarecdn.com') {
         const uuid = url.pathname.split('/')[1];
         const imgWidths = [400, 600, 800, 1000];
         const getImgUrl = (width, doubleRes = false) => (
@@ -116,19 +113,27 @@ function fetchRequest(request) {
             return undefined;
           });
       }
-      if (!res) {
-        return caches.match('/offline').then(r => r);
-      }
-      return res;
+      return caches.match('/offline').then(r => r);
     }));
+}
+
+function cacheFirst(e) {
+  return caches.match(e.request).then((res) => {
+    if (res) {
+      return res;
+    }
+    return fetchRequest(e.request);
+  });
 }
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method === 'GET') {
     const url = new URL(e.request.url);
     const local = url.origin === location.origin;
-    if (local) {
+    const cacheLocal = ['/scripts/nosleep.js'];
+    if (local && !cacheLocal.includes(url.pathname)) {
       if (isRecipeView(url) && 'template' in self) {
+        // get server response with 5 second timeout, render offline otherwise
         const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
         const renderOffline = async () => {
           await sleep(5000);
@@ -140,14 +145,7 @@ self.addEventListener('fetch', (e) => {
         e.respondWith(fetchRequest(e.request));
       }
     } else {
-      e.respondWith(
-        caches.match(e.request).then((res) => {
-          if (res) {
-            return res;
-          }
-          return fetchRequest(e.request);
-        }),
-      );
+      e.respondWith(cacheFirst(e));
     }
   }
 });
