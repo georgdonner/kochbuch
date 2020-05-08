9b928cb287760053d094969f45f59a423d118283
Georg Donner <georgdonner@posteo.de>
2020-05-08 19:59:03 +0200

    Lazy load recipe form

--- client/src/app.js
+++ client/src/app.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/login/Login';
 import Recipes from './pages/recipes/Recipes';
 import Recipe from './pages/recipe/Recipe';
-import RecipeForm from './pages/recipeForm/RecipeForm';
 import List from './pages/list/List';
 import Plan from './pages/plan/Plan';
 import PlanForm from './pages/planForm/PlanForm';
 import Settings from './pages/settings/Settings';
+import Loading from './components/Loading';
+
+const RecipeForm = lazy(() => import('./pages/recipeForm/RecipeForm'));
 
 export default () => (
-  <Router>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/list" component={List} />
-      <Route path="/plan/new" component={PlanForm} />
-      <Route path="/plan/:id/edit" component={PlanForm} />
-      <Route path="/plan" component={Plan} />
-      <Route path="/recipes/new" component={RecipeForm} />
-      <Route path="/recipe/:id/edit" component={RecipeForm} />
-      <Route path="/recipe/:id" component={Recipe} />
-      <Route path="/settings" component={Settings} />
-      <Route path="/" component={Recipes} />
-    </Switch>
-  </Router>
+  <Suspense fallback={<Loading />}>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/list" component={List} />
+        <Route path="/plan/new" component={PlanForm} />
+        <Route path="/plan/:id/edit" component={PlanForm} />
+        <Route path="/plan" component={Plan} />
+        <Route path="/recipes/new" component={RecipeForm} />
+        <Route path="/recipe/:id/edit" component={RecipeForm} />
+        <Route path="/recipe/:id" component={Recipe} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/" component={Recipes} />
+      </Switch>
+    </Router>
+  </Suspense>
 );
